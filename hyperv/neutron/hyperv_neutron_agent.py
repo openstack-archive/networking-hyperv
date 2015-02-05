bclau/neutron-hyperv-agent

Adds logging implementation
Adds logging implementation so it won't have to be
imported from neutron.

Change-Id: I7255653dce164f8d655c548465d4a6547981321d

cloudbase/neutron/hyperv/hyperv_neutron_agent.py
@@ -17,9 +17,8 @@
 import re
 import time
 
-from neutron.openstack.common import log as logging
-
 from cloudbase.common.i18n import _LE, _LI
+from cloudbase.common import log as logging
 from cloudbase.neutron.hyperv import utils
 from cloudbase.neutron.hyperv import utilsfactory
 from cloudbase.neutron.hyperv import constants

cloudbase/neutron/hyperv/security_groups_driver.py
@@ -13,9 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.openstack.common import log as logging
-
 from cloudbase.common.i18n import _LE, _LI
+from cloudbase.common import log as logging
 from cloudbase.neutron.hyperv import utilsfactory
 from cloudbase.neutron.hyperv import utilsv2
 

cloudbase/neutron/hyperv/utilsfactory.py
@@ -17,9 +17,8 @@ import sys
 
 from oslo.config import cfg
 
-from neutron.openstack.common import log as logging
-
 from cloudbase.common.i18n import _LW
+from cloudbase.common import log as logging
 from cloudbase.neutron.hyperv import utils
 from cloudbase.neutron.hyperv import utilsv2
 

openstack-archive/networking-hyperv

hyperv/neutron/hyperv_neutron_agent.py
@@ -17,9 +17,8 @@
 import re
 import time
 
-from neutron.openstack.common import log as logging
-
 from hyperv.common.i18n import _LE, _LI
+from hyperv.common import log as logging
 from hyperv.neutron import constants
 from hyperv.neutron import utils
 from hyperv.neutron import utilsfactory

hyperv/neutron/security_groups_driver.py
@@ -13,9 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.openstack.common import log as logging
-
 from hyperv.common.i18n import _LE, _LI
+from hyperv.common import log as logging
 from hyperv.neutron import utilsfactory
 from hyperv.neutron import utilsv2
 

hyperv/neutron/utilsfactory.py
@@ -17,9 +17,8 @@ import sys
 
 from oslo.config import cfg
 
-from neutron.openstack.common import log as logging
-
 from hyperv.common.i18n import _LW
+from hyperv.common import log as logging
 from hyperv.neutron import utils
 from hyperv.neutron import utilsv2
 