e287b37f8b1c19dffac2b3d42f6cb4c13512316e
Claudiu Belu <cbelu@cloudbasesolutions.com>
2015-03-09 20:50:21 +0200

    Adds SecurityGroupRule and Generator
    SecurityGroups Driver has proven difficult to maintain,
    mainly because of the feature works quite differently on
    Windows / Hyper-V Server 2012 and Windows / Hyper-V Server 2012 R2
    and different cases and exceptions on both systems.
    
    This commit adds the base classes needed to refactor
    Security Groups.
    
    Change-Id: I99fa8cfae2d105d89be930c2129405aff7fa3426
    (cherry picked from commit 4bdfb93fb2c38875e6e18f98b083a90d32c5eb83)

--- hyperv/neutron/security_groups_driver.py
+++ hyperv/neutron/security_groups_driver.py
@@ -22,22 +22,26 @@ from hyperv.neutron import utilsv2
 
 LOG = logging.getLogger(__name__)
 
+ACL_PROP_MAP = {
+    'direction': {'ingress': utilsv2.HyperVUtilsV2._ACL_DIR_IN,
+                  'egress': utilsv2.HyperVUtilsV2._ACL_DIR_OUT},
+    'ethertype': {'IPv4': utilsv2.HyperVUtilsV2._ACL_TYPE_IPV4,
+                  'IPv6': utilsv2.HyperVUtilsV2._ACL_TYPE_IPV6},
+    'protocol': {'tcp': utilsv2.HyperVUtilsV2._TCP_PROTOCOL,
+                 'udp': utilsv2.HyperVUtilsV2._UDP_PROTOCOL,
+                 'icmp': utilsv2.HyperVUtilsV2._ICMP_PROTOCOL},
+    'action': {'allow': utilsv2.HyperVUtilsV2._ACL_ACTION_ALLOW,
+               'deny': utilsv2.HyperVUtilsV2._ACL_ACTION_DENY},
+    'default': "ANY",
+    'address_default': {'IPv4': '0.0.0.0/0', 'IPv6': '::/0'}
+}
+
 
 class HyperVSecurityGroupsDriverMixin(object):
     """Security Groups Driver.
 
     Security Groups implementation for Hyper-V VMs.
     """
-
-    _ACL_PROP_MAP = {
-        'direction': {'ingress': utilsv2.HyperVUtilsV2._ACL_DIR_IN,
-                      'egress': utilsv2.HyperVUtilsV2._ACL_DIR_OUT},
-        'ethertype': {'IPv4': utilsv2.HyperVUtilsV2._ACL_TYPE_IPV4,
-                      'IPv6': utilsv2.HyperVUtilsV2._ACL_TYPE_IPV6},
-        'protocol': {'icmp': utilsv2.HyperVUtilsV2._ICMP_PROTOCOL},
-        'default': "ANY",
-        'address_default': {'IPv4': '0.0.0.0/0', 'IPv6': '::/0'}
-    }
 
     def __init__(self):
         self._utils = utilsfactory.get_hypervutils()
@@ -83,11 +87,11 @@ class HyperVSecurityGroupsDriverMixin(object):
             local_port = '%s-%s' % (rule['port_range_min'],
                                     rule['port_range_max'])
         else:
-            local_port = self._ACL_PROP_MAP['default']
+            local_port = ACL_PROP_MAP['default']
 
         return {
-            'direction': self._ACL_PROP_MAP['direction'][rule['direction']],
-            'acl_type': self._ACL_PROP_MAP['ethertype'][rule['ethertype']],
+            'direction': ACL_PROP_MAP['direction'][rule['direction']],
+            'acl_type': ACL_PROP_MAP['ethertype'][rule['ethertype']],
             'local_port': local_port,
             'protocol': self._get_rule_protocol(rule),
             'remote_address': self._get_rule_remote_address(rule)
@@ -135,16 +139,166 @@ class HyperVSecurityGroupsDriverMixin(object):
 
         if ip_prefix in rule:
             return rule[ip_prefix]
-        return self._ACL_PROP_MAP['address_default'][rule['ethertype']]
+        return ACL_PROP_MAP['address_default'][rule['ethertype']]
 
     def _get_rule_protocol(self, rule):
         protocol = self._get_rule_prop_or_default(rule, 'protocol')
-        if protocol in self._ACL_PROP_MAP['protocol'].keys():
-            return self._ACL_PROP_MAP['protocol'][protocol]
+        if protocol in ACL_PROP_MAP['protocol'].keys():
+            return ACL_PROP_MAP['protocol'][protocol]
 
         return protocol
 
     def _get_rule_prop_or_default(self, rule, prop):
         if prop in rule:
             return rule[prop]
-        return self._ACL_PROP_MAP['default']
+        return ACL_PROP_MAP['default']
+
+
+class SecurityGroupRuleGenerator(object):
+
+    def create_security_group_rules(self, rules):
+        security_group_rules = []
+        for rule in rules:
+            security_group_rules.extend(self.create_security_group_rule(rule))
+        return security_group_rules
+
+    def create_security_group_rule(self, rule):
+        # TODO(claudiub): implement
+        pass
+
+    def _get_rule_remote_address(self, rule):
+        if rule['direction'] is 'ingress':
+            ip_prefix = 'source_ip_prefix'
+        else:
+            ip_prefix = 'dest_ip_prefix'
+
+        if ip_prefix in rule:
+            return rule[ip_prefix]
+        return ACL_PROP_MAP['address_default'][rule['ethertype']]
+
+
+class SecurityGroupRuleGeneratorR2(SecurityGroupRuleGenerator):
+
+    def create_security_group_rule(self, rule):
+        local_port = self._get_rule_port_range(rule)
+        direction = ACL_PROP_MAP['direction'][rule['direction']]
+        remote_address = self._get_rule_remote_address(rule)
+        protocol = self._get_rule_protocol(rule)
+        if protocol == ACL_PROP_MAP['default']:
+            # ANY protocols must be split up, to make stateful rules.
+            protocols = ACL_PROP_MAP['protocol'].values()
+        else:
+            protocols = [protocol]
+
+        sg_rules = [SecurityGroupRuleR2(direction=direction,
+                                        local_port=local_port,
+                                        protocol=proto,
+                                        remote_addr=remote_address)
+                    for proto in protocols]
+
+        if (direction == ACL_PROP_MAP['direction']['egress'] and
+                protocol == ACL_PROP_MAP['default']):
+            # ICMP rules cannot be set as stateful. Create an Inbound
+            # equivalent rule, so PING REPLY can be accepted.
+            sg_rules.append(SecurityGroupRuleR2(
+                direction=ACL_PROP_MAP['direction']['ingress'],
+                local_port='',
+                protocol=ACL_PROP_MAP['protocol']['icmp'],
+                remote_addr=remote_address))
+        elif protocol == ACL_PROP_MAP['protocol']['icmp']:
+            # If ICMP rule is added in one direction, create an equivalent
+            # rule for the other direction.
+            if direction == ACL_PROP_MAP['direction']['ingress']:
+                direction = ACL_PROP_MAP['direction']['egress']
+            else:
+                direction = ACL_PROP_MAP['direction']['ingress']
+            sg_rules.append(SecurityGroupRuleR2(
+                direction=direction,
+                local_port='',
+                protocol=protocol,
+                remote_addr=remote_address))
+
+        return sg_rules
+
+    def create_default_sg_rules(self):
+        ip_type_pairs = [(ACL_PROP_MAP['ethertype'][ip],
+                          ACL_PROP_MAP['address_default'][ip])
+                         for ip in ACL_PROP_MAP['ethertype'].keys()]
+
+        action = ACL_PROP_MAP['action']['deny']
+        port = ACL_PROP_MAP['default']
+        sg_rules = []
+        for direction in ACL_PROP_MAP['direction'].values():
+            for protocol in ACL_PROP_MAP['protocol'].values():
+                for acl_type, address in ip_type_pairs:
+                    sg_rules.append(SecurityGroupRuleR2(direction=direction,
+                                                        local_port=port,
+                                                        protocol=protocol,
+                                                        remote_addr=address,
+                                                        action=action))
+        return sg_rules
+
+    def compute_new_rules_add(self, old_rules, new_rules):
+        add_rules = [r for r in new_rules if r not in old_rules]
+        return add_rules, []
+
+    def _get_rule_port_range(self, rule):
+        if 'port_range_min' in rule and 'port_range_max' in rule:
+            return '%s-%s' % (rule['port_range_min'],
+                              rule['port_range_max'])
+        return ACL_PROP_MAP['default']
+
+    def _get_rule_protocol(self, rule):
+        protocol = self._get_rule_prop_or_default(rule, 'protocol')
+        if protocol in ACL_PROP_MAP['protocol'].keys():
+            return ACL_PROP_MAP['protocol'][protocol]
+
+        return protocol
+
+    def _get_rule_prop_or_default(self, rule, prop):
+        if prop in rule:
+            return rule[prop]
+        return ACL_PROP_MAP['default']
+
+
+class SecurityGroupRuleBase(object):
+
+    _FIELDS = []
+
+    def __eq__(self, obj):
+        for f in self._FIELDS:
+            if not hasattr(obj, f) or getattr(obj, f) != getattr(self, f):
+                return False
+        return True
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def __repr__(self):
+        return str(self)
+
+    def to_dict(self):
+        return dict((field, getattr(self, field)) for field in self._FIELDS)
+
+
+class SecurityGroupRuleR2(SecurityGroupRuleBase):
+
+    _FIELDS = ["Direction", "Action", "LocalPort", "Protocol",
+               "RemoteIPAddress", "Stateful", "IdleSessionTimeout"]
+
+    IdleSessionTimeout = 0
+
+    def __init__(self, direction, local_port, protocol, remote_addr,
+                 action=ACL_PROP_MAP['action']['allow']):
+        is_not_icmp = protocol is not ACL_PROP_MAP['protocol']['icmp']
+
+        self.Direction = direction
+        self.Action = action
+        self.LocalPort = str(local_port) if is_not_icmp else ''
+        self.Protocol = protocol
+        self.RemoteIPAddress = remote_addr
+        self.Stateful = (is_not_icmp and
+                         action is not ACL_PROP_MAP['action']['deny'])
+
+    def __lt__(self, obj):
+        return self.Protocol > obj.Protocol
