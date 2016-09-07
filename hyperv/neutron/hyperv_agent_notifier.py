d04ffc7b492b3da7f35004f87f373dc6636c4fe9
Cao Xuan Hoang <hoangcx@vn.fujitsu.com>
2016-09-07 13:46:00 +0700

    TrivialFix: Remove logging import unused
    This patch removes logging import unused in
    hyperv/neutron/hyperv_agent_notifier.py
    
    Change-Id: I0d6fc34118546b97b117c82b891948332c8af085

--- hyperv/neutron/hyperv_agent_notifier.py
+++ hyperv/neutron/hyperv_agent_notifier.py
@@ -13,11 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from oslo_log import log as logging
-
 from hyperv.neutron import constants
-
-LOG = logging.getLogger(__name__)
 
 
 def get_topic_name(prefix, table, operation):
