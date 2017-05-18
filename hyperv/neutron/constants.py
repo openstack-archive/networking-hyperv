968911ba27e725d9d34b7cbb53c6aeedaec2b9ea
Claudiu Belu <cbelu@cloudbasesolutions.com>
2017-05-18 18:56:41 +0300

    Fixes Windows Server 2016 Network Controller constants
    In order to properly use the Windows Server 2016 Network Controller,
    a switch port must have set a port profile with some very specific
    constants, otherwise it won't work. [1]
    
    [1] https://docs.microsoft.com/en-us/windows-server/networking/sdn/manage/create-a-tenant-vm
    
    Change-Id: Ic3788ed8945e337ab9bd345731be8792c0d346b7
    Partially-Implements: blueprint hyperv-network-virtualization-support

--- hyperv/neutron/constants.py
+++ hyperv/neutron/constants.py
@@ -34,10 +34,15 @@ TYPE_NVGRE = 'gre'
 
 IPV4_DEFAULT = '0.0.0.0'
 
-NET_CFG_INSTANCE_ID = "{00000000-0000-0000-0000-000000000000}"
-CDN_LABEL_STRING = "OpenStackHyperVCDN"
+# Windows Server 2016 Network Controller related constants.
+# NOTE(claudiub): These constants HAVE to be defined exactly like this,
+# otherwise networking using the Windows Server 2016 Network Controller won't
+# work.
+# https://docs.microsoft.com/en-us/windows-server/networking/sdn/manage/create-a-tenant-vm  # noqa
+NET_CFG_INSTANCE_ID = "{56785678-a0e5-4a26-bc9b-c0cba27311a3}"
+CDN_LABEL_STRING = "OpenStackCdn"
 CDN_LABEL_ID = 1111
 PROFILE_NAME = "OpenStackProfile"
-VENDOR_ID = "{00000000-0000-0000-0000-000000000000}"
+VENDOR_ID = "{1FA41B39-B444-4E43-B35A-E1F7985FD548}"
 VENDOR_NAME = "NetworkController"
 PROFILE_DATA = 1
